78d5ba11dd67d38da740da15470e33d3af7e1e37
Benjamin Taylor <staylor.ben@gmail.com>
2018-05-21 22:13:11 -0700

    replicated demo functionality

--- scripts/index.js
+++ scripts/index.js
@@ -3,5 +3,27 @@
 var app = app || {};
 
 (function(module) {
+  let productionApiUrl = 'https://bt-mg-booklist.herokuapp.com';
+  let developmentApiUrl = 'http://localhost:3000';
 
+  //Create a property called isProduction to evaluate your environment based on the browser URL.
+  module.isProduction = window.location.protocol === 'https'
+
+  //Use this property to set app.ENVIRONMENT.apiUrl.
+  module.ENVIRONMENT = {
+    apiUrl: module.isProduction ? productionApiUrl : developmentApiUrl
+  };
+
+  //Create a .showOnly method to reveal the containters of your single-page app
+  module.showOnly = (selector) => {
+    $('.container').hide();
+    $(selector).show();
+  }
+
+  //Create a .render method to compile your Handlebars template
+  module.render = (templateId, data) => {
+    let template = Handlebars.compile($(`#${templateId}`).text());
+    return template(data);
+  }
+  
 })(app)
--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -3,5 +3,20 @@
 var app = app || {};
 
 (function(module) {
+  //Define a global variable called bookView and assign an empty object literal as its value.
+  const bookView = {};
 
+  //Define a bookView.initIndexPage method which hides any element with a class of container, shows any element with a class of book-view, and maps over the Book instances stored in Book.all to render each and append them to an element with the id of book-list.
+  bookView.initIndexPage = function() {
+    app.showOnly('.book-view');
+
+    app.Book.all.map(book => $('#book-list').append(book.toHtml()));
+  }
+
+  module.bookView = bookView;
 })(app)
+
+//Using jQuery's Document.ready functionality, invoke Book.fetchAll when the DOM has loaded, and pass bookView.initIndexPage as it's argument.
+$(function() {
+  app.Book.fetchAll(app.bookView.initIndexPage);
+})
